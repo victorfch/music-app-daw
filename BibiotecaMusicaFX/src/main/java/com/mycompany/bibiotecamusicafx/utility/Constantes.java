d609f87a660bb0d582b325241539aca6f30831a6
VictorFCh <vfariachavez@gmail.com>
2020-05-07 18:04:44 +0100

    arreglado patron fecha

--- BibiotecaMusicaFX/src/main/java/com/mycompany/bibiotecamusicafx/utility/Constantes.java
+++ BibiotecaMusicaFX/src/main/java/com/mycompany/bibiotecamusicafx/utility/Constantes.java
@@ -13,6 +13,6 @@ public class Constantes {
     public static final String ETIQUETA_NACIONALIDAD = "Nacionalidad";
     public static final String ETIQUETA_EDAD = "Años";
     public static final String MSG_NO_HAY_ARTISTAS = "No hay artistas añadidos";
-    public static final String PATRON_FECHA = "dd/mm/yyyy";
+    public static final String PATRON_FECHA = "dd/MM/yyyy";
     public static final String MSG_COMPLETAR_CAMPOS = "Por favor, complete todos los campos";
 }
